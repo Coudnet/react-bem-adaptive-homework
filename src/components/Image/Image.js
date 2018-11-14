f70b7607efe9ac5745c7ef5ab214eba89dfd8d81
Гриша <greha1307@gmail.com>
2018-11-14 17:44:47 +0300

    Поправлены стили для контролов температуры и изображения в соответствии с БЭМ

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,10 +3,14 @@ import './Image.scss'
 import image1x from './Img1x.png';
 import image2x from './Img2x.png';
 import image3x from './Img3x.png';
+import {cn} from '@bem-react/classname';
+
+const cnImageControl = cn('ImageControl');
+
 
 const Image = (props) => {
     return (
-        <div className="card-content-control-image">
+        <div className={cnImageControl()}>
             <img src={image1x} alt="Пылесос застряяяял" />
         </div>
     );
--- src/components/Temperature/Temperature.js
+++ src/components/Temperature/Temperature.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Temperature.scss'
+import {cn} from '@bem-react/classname';
+
+const cnTemperatureControl = cn('TemperatureControl');
 
 const Temperature = (props) => {
     return (
-        <div className="card-content-control-thermal">
-            <div className="temprature-value">Температура <b>{props.data.temperature}&deg;C</b></div>
-            <div className="humidity-value">Влажность <b>{props.data.humidity}%</b></div>
+        <div className={cnTemperatureControl()}>
+            <div className={cnTemperatureControl('Temperature')}>Температура <b>{props.data.temperature}&deg;C</b></div>
+            <div className={cnTemperatureControl('Humidity')}>Влажность <b>{props.data.humidity}%</b></div>
         </div>
     );
 };
